68cf67671d98c18e17cdde40a5a959c29545634d
xaviguasch <xaviguasch@gmail.com>
2019-04-27 18:27:34 +0200

    starter files

---
+++ Hello.js
@@ -0,0 +1,9 @@
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    )
+  }
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
